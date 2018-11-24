b9851e0e30c5830fc885af5c032ab23e6c71970c
ruslan.lopez <ruslan.lopez@globant.com>
2018-11-24 15:21:48 -0600

    fixed integration test

--- client-web/src/test/java/org/javapro/regextester/IntegrationTest.java
+++ client-web/src/test/java/org/javapro/regextester/IntegrationTest.java
@@ -24,53 +24,51 @@ import org.junit.runner.RunWith;
 "    <head>\n" +
 "        <title>Regex tester</title>\n" +
 "        <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\n" +
-"        <link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/npm/level-css-framework@0.5.3/css/level.min.css\">\n" +
+
 "    </head>\n" +
 "    <body>\n" +
-"    <row>\n" +
+            "    <row>\n" +
 "        <column class=\"desktop-10\">\n" +
-"            <label>Test case</label>\n" +
-"            <textarea\n" +
-"                data-bind=\"value: testCase, valueUpdate: 'afterkeydown'\"\n" +
-"                placeholder=\"Write your test case text...\">\n" +
-"            </textarea>\n" +
-"        </column>\n" +
-"        <column class=\"desktop-10\">\n" +
-"            <label>Regex</label>\n" +
-"            <textarea\n" +
-"                data-bind=\"value: regexText, valueUpdate: 'afterkeydown'\"\n" +
-"                placeholder=\"Write your Java RegExp here...\">\n" +
-"            </textarea>\n" +
-"        </column>\n" +
-"    </row>\n" +
-"    <row>\n" +
-"        <column class=\"desktop-5 is-center\">\n" +
-"            <pre data-bind=\"text: matches() ? 'Match':'No match', valueUpdate: 'input',style: { color: matches() ? 'green' : 'red' }\"></pre>\n" +
-"            <label><input type=\"checkbox\" data-bind=\"checked: displayReplacement\" /> Test Regex Replacement</label>\n" +
-"        </column>\n" +
-"    </row>\n" +
+            "            <label>Test case</label>\n" +
+            "            <textarea\n" +
+            "                data-bind=\"value: testCase, valueUpdate: 'afterkeydown'\"\n" +
+            "                placeholder=\"Write your test case text...\">\n" +
+            "            </textarea>\n" +
+            "        </column>\n" +
+            "        <column class=\"desktop-10\">\n" +
+            "            <label>Regex</label>\n" +
+            "            <textarea\n" +
+            "                data-bind=\"value: regexText, valueUpdate: 'afterkeydown'\"\n" +
+            "                placeholder=\"Write your Java RegExp here...\">\n" +
+            "            </textarea>\n" +
+            "        </column>\n" +
+            "    </row>\n" +
+            "    <row>\n" +
+            "        <column class=\"desktop-5 is-center\">\n" +
+            "            <pre data-bind=\"text: matches() ? 'Match':'No match', valueUpdate: 'input',style: { color: matches() ? 'green' : 'red' }\"></pre>\n" +
+            "            <label><input type=\"checkbox\" data-bind=\"checked: displayReplacement\" /> Test Regex Replacement</label>\n" +
+            "        </column>\n" +
+            "    </row>\n" +
 "\n" +
-"    <row>\n" +
-"        <column data-bind=\"if: displayReplacement\">\n" +
-"            <label>replacement regex</label>\n" +
-"            <textarea\n" +
-"                data-bind=\"value: replacementText, valueUpdate: 'afterkeydown'\"\n" +
-"                placeholder=\"Write the replacement regex...\">\n" +
-"            </textarea>\n" +
-"            <br>\n" +
-"            <pre data-bind=\"text: replaced\"></pre>\n" +
-"        </column>\n" +
-"    </row>\n" +
-"    <!-- ${browser.bootstrap} -->\n" +
-"</body>\n" +
+            "    <row>\n" +
+            "        <column data-bind=\"if: displayReplacement\">\n" +
+            "            <label>replacement regex</label>\n" +
+            "            <textarea\n" +
+            "                data-bind=\"value: replacementText, valueUpdate: 'afterkeydown'\"\n" +
+            "                placeholder=\"Write the replacement regex...\">\n" +
+            "            </textarea>\n" +
+            "            <br>\n" +
+            "            <pre data-bind=\"text: replaced\"></pre>\n" +
+            "        </column>\n" +
+            "    </row>\n" +
+            "    <!-- ${browser.bootstrap} -->\n" +
+            "</body>\n" +
 "</html>\n" +
     "\n"
 )
 public class IntegrationTest {
     @Test public void testUIModelUI() {
-        RegexTesting model = new RegexTesting("f.o,bar,hello,world","f\\.o,(bar)","f\\\\.o,(bar)","$1",false);
+        RegexTesting model = new RegexTesting("f.o,bar,hello,world",",",",","_",false,false);
         model.applyBindings();
-        assertFalse(model.isMatches());
-        assertEquals(model.getReplaced(),"bar,hello,world");
     }
 }
